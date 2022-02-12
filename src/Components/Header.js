dad8fc328ab85fc84935cc88cce907e8cd695285
Arnita <arnitucis@inbox.lv>
2022-02-12 11:07:11 +0200

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,31 +1,36 @@
 import { Link } from "react-router-dom";
 
 function Header() {
-    return (
-        <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <div className="container-fluid">
-                    <Link className="navbar-brand" to="/">Logo</Link>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav">
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/">Home</Link>
-                                
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/categories">Categories</Link>
-                                
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/cart">Cart</Link>
-                                
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            Logo
+          </Link>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/categories">
+                  Categories
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/cart">
+                  Cart
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
-    )
+      </nav>
+    </div>
+  );
 }
 
 export default Header;
