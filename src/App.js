9512a18d95377545a0a9f1b64780ddb00fd5d70b
Arnita <arnitucis@inbox.lv>
2022-02-12 14:25:58 +0200

    update c ard
    hgv

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import Content from "./Components/Content";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 
-
 function App() {
   return (
     <div>
--- src/Components/Content.js
+++ src/Components/Content.js
@@ -4,19 +4,24 @@ import Category123 from "../Pages/Category123";
 import ProductX from "../Pages/ProductX";
 import Cart from "../Pages/Cart";
 import Home from "../Pages/Home";
+import Products from "./Products";
+import Address from "./Address";
 
 function Content() {
-    return (
-        <div className="container">
-            <Routes>
-                <Route path="/" element={<Home />}/>
-                <Route path="/features" element={<Categories />}/>
-                <Route path="/features" element={<Category123/>}/>
-                <Route path="/features" element={<ProductX/>}/>\
-                <Route path="/features" element={<Cart/>}/>
-            </Routes>
-        </div>
-    )
+  return (
+    <div className="container">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/category123" element={<Category123 />} />
+        <Route path="/productX" element={<ProductX />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/cart/products" element={<Products />} />
+        <Route path="/cart/address" element={<Address />} />
+        <Route path="/cartsummary" element={<Summary />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default Content;
