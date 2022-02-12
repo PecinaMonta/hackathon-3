359e793a7cbbb2d43d3545d8486a785f90908142
Arnita <arnitucis@inbox.lv>
2022-02-12 14:08:01 +0200

    update cart tabs

---
+++ src/Components/Summary.js
@@ -0,0 +1,12 @@
+import Cart from "../Pages/Cart";
+function Summary() {
+  return (
+    <footer>
+      <div className="container">
+        <h1 className="productTab">This is a summary tab</h1>
+      </div>
+    </footer>
+  );
+}
+
+export default Summary;
