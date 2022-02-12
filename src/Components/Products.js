5a8f2bd76f8dbeb180e54f3327582c1fa98d1984
PetersoneIeva <ieva.petersone@va.lv>
2022-02-12 23:07:04 +0200

    Tab

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,10 +1,9 @@
-import Cart from "../Pages/Cart";
+
 
 function Products() {
   return (
     <footer>
       <div className="container">
-        <Cart />
         <h1 className="productTab">This is a product tab</h1>
       </div>
     </footer>
