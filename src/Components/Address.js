9d12f2f4874b6be4ebe1068cb573e542a25aa084
Arnita <arnitucis@inbox.lv>
2022-02-14 00:01:11 +0200

    Cart update
    col and row cleanup

--- src/Components/Address.js
+++ src/Components/Address.js
@@ -14,32 +14,36 @@ function Address() {
   return (
     <div className="containerFluid">
       <CartTabs />
-      <div className="row">
-        <div className="col">
+      <div className="row mt-1">
+        <div className="col mb-4">
           <h2>Shipping address:</h2>
-          <div className="input-group input-group-lg">
-            <input
-              value={addressShip}
-              onChange={updateAddressShip}
-              type="text"
-              className="form-control"
-              aria-label="Sizing example input"
-              aria-describedby="inputGroup-sizing-lg"
-            />
+          <div className="row mt-1">
+            <div className="input-group input-group-lg">
+              <input
+                value={addressShip}
+                onChange={updateAddressShip}
+                type="text"
+                className="form-control"
+                aria-label="Sizing example input"
+                aria-describedby="inputGroup-sizing-lg"
+              />
+            </div>
           </div>
         </div>
-        <div className="col">
+        <div className="col mt-6 mb-4">
           <h2>Billing address:</h2>
-          <div className="input-group input-group-lg">
-            <input
-              value={addressBill}
-              onChange={updateAddressBill}
-              type="text"
-              className="form-control"
-              aria-label="Sizing example input"
-              aria-describedby="inputGroup-sizing-lg"
-              id="addressInput"
-            />
+          <div className="row mt-1">
+            <div className="input-group input-group-lg">
+              <input
+                value={addressBill}
+                onChange={updateAddressBill}
+                type="text"
+                className="form-control"
+                aria-label="Sizing example input"
+                aria-describedby="inputGroup-sizing-lg"
+                id="addressInput"
+              />
+            </div>
           </div>
         </div>
       </div>
