194986970c86cbf957e9b3a115358d55b57b51e9
Monta Pecina <montapecina@gmail.com>
2022-02-13 18:44:58 +0100

    update

--- src/Components/CategoryList.js
+++ src/Components/CategoryList.js
@@ -5,13 +5,12 @@ function CategoryList(props) {
     const description = props.description;
     
 
-
     return (
         <div className="card">
             <div className="card-body">
                 <h5 className="card-title">{title}</h5>
                 <p className="card-text">{description}</p>
-                <Link to="/" class="btn btn-secondary btn-sm" role="button" aria-disabled="true">Explore more</Link>
+                <Link to="/categories" className="btn btn-secondary btn-sm" role="button" aria-disabled="true">Explore more</Link>
             </div>
         </div>
     )
